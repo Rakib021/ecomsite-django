f25cd7cfcf21bf9eeb6519f1f17830e7a2b77b64
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-22 00:33:41 +0600

    playing with templates

--- ecomsite/settings.py
+++ ecomsite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'shop',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecomsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR  /'static'
+STATICFILES_DIRS = [
+    'ecomsite/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- shop/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-  title = models.CharField(max_length=200)
-  price = models.FloatField()
-  discount_price = models.FloatField()
-  category = models.CharField(max_length=200)
-  description = models.TextField()
